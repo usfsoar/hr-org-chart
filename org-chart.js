4a8c32f34131da2b565c00100dfd4d830b7d9914
Ian Sanders <iansan5653@gmail.com>
2018-08-17 11:38:21 -0400

    Fix eslint erros

--- org-chart.js
+++ org-chart.js
@@ -1,5 +1,6 @@
 /** Location of SOAR's Position Database on Google Sheets. */
 const POSITION_DB_URL = 'https://docs.google.com/spreadsheets/d/1ugs5aryRt4Xii2kVJ6OUF4edFRDFaesFtmJ6yTLr6tc/gviz/tq?gid=Positions&headers=1';
+/** Indexes of columns in the positions sheet. */
 const COL_INDICES = {
   TEAM: 0,
   POSITION: 1,
@@ -8,23 +9,25 @@ const COL_INDICES = {
   RESPORTS_TO: 4,
   ACCEPTING_APPLICATIONS: 5,
   POSITION_DESCRIPTION: 6,
-  COMMENTS: 7
+  COMMENTS: 7,
 };
+/** HTML ID of the org chart container. */
 const CONTAINER_ID = 'orgChartContainer';
+/** URL to direct prospective applicants to. */
 const ACCEPTING_APPLICATIONS_URL = 'http://usfsoar.com/positions';
 
 google.charts.load('current', {packages: ['orgchart']});
 google.charts.setOnLoadCallback(loadData);
 
-/** Initialize the Google Chart (callback for Charts library load). */
+/** Load the positions data from Google Sheets. */
 function loadData() {
-  var query = new google.visualization.Query(POSITION_DB_URL);
+  let query = new google.visualization.Query(POSITION_DB_URL);
   query.send(handleDataResponse);
 }
 
 /**
  * Process and filter the response data.
- * @param {google.visualization.QueryResponse} response The Query response.
+ * @param {google.visualization.QueryResponse} response The response object.
  */
 function handleDataResponse(response) {
   let data = response.getDataTable();
@@ -35,15 +38,29 @@ function handleDataResponse(response) {
     let positionName = data.getValue(row, COL_INDICES.POSITION);
     let positionTeam = data.getValue(row, COL_INDICES.TEAM);
     let positionHeldBy = data.getValue(row, COL_INDICES.HELD_BY);
-    let positionOpen = data.getValue(row, COL_INDICES.ACCEPTING_APPLICATIONS) === 'Yes';
-    let formatted = `<div class="orgChart-node-flex">
-                      <span class="orgChart-node-team">${positionTeam}</span>
-                      <span class="orgChart-node-position">${positionName}</span>
-                      ${positionHeldBy ? `<span class="orgChart-node-heldBy">${positionHeldBy}</span>` : ''}
-                      ${positionOpen ? `<span class="orgChart-node-open"><a href="${ACCEPTING_APPLICATIONS_URL}">Open - Apply Now!</a></span>` : ''}
-                      ${!positionHeldBy && !positionOpen ? `<span class="orgChart-node-spaceFiller"> </span>` : ``}
-                     </div>`;
-    data.setCell(row, newColumnIndex, data.getValue(row, COL_INDICES.UNIQUE_NAME), formatted);
+    let positionOpen =
+        data.getValue(row, COL_INDICES.ACCEPTING_APPLICATIONS) === 'Yes';
+    let formatted =
+        `<div class="orgChart-node-flex">
+          <span class="orgChart-node-team">${positionTeam}</span>
+          <span class="orgChart-node-position">${positionName}</span>
+          ${positionHeldBy
+              ? `<span class="orgChart-node-heldBy">${positionHeldBy}</span>`
+              : ''}
+          ${positionOpen
+              ? `<span class="orgChart-node-open">
+                  <a href="${ACCEPTING_APPLICATIONS_URL}">Open - Apply Now!</a>
+                </span>`
+              : ''}
+          ${(!positionHeldBy && !positionOpen)
+              ? `<span class="orgChart-node-spaceFiller"> </span>`
+              : ``}
+        </div>`;
+
+    data.setCell(
+      row,
+      newColumnIndex,
+      data.getValue(row, COL_INDICES.UNIQUE_NAME), formatted);
   }
 
   let unfilteredDataView = new google.visualization.DataView(data);
@@ -51,15 +68,16 @@ function handleDataResponse(response) {
     newColumnIndex,
     COL_INDICES.RESPORTS_TO,
     COL_INDICES.HELD_BY,
-    COL_INDICES.TEAM // For filtering
+    COL_INDICES.TEAM, // For filtering; ignored by org chart
   ]);
 
   initOrgChart(unfilteredDataView);
 }
 
 /**
- * Create the org chart object.
- * @param {google.visualization.DataView} data
+ * Create the org chart object and then draw the chart and initialize the filter
+ * buttons.
+ * @param {google.visualization.DataView} unfilteredData
  */
 function initOrgChart(unfilteredData) {
   const container = document.getElementById(CONTAINER_ID);
@@ -79,21 +97,22 @@ function drawOrgChart(chart, data) {
     allowCollapse: true,
     allowHtml: true,
     nodeClass: 'orgChart-node',
-    selectedNodeClass: 'orgChart-node--selected'
+    selectedNodeClass: 'orgChart-node--selected',
   });
 }
 
 /**
  * Filter the data by team name and redraw chart.
  * @param {google.visualization.OrgChart} chart The chart object.
- * @param {google.visualization.DataView} originalDataView The chart's unfiltered
- * data view.
- * @param {?string} [teamName] The exact name of the team to keep showing.
+ * @param {google.visualization.DataView} unfilteredData The chart's
+ * unfiltered data view.
+ * @param {?string} [teamName] The exact name of the team to show, or a falsy
+ * value to show all positions.
  */
 function filterChartData(chart, unfilteredData, teamName) {
   let filteredDataView = new google.visualization.DataView(unfilteredData);
 
-  if(teamName) {
+  if (teamName) {
     let matchingRows = filteredDataView.getFilteredRows([{
       column: 3,
       value: teamName,
@@ -119,14 +138,15 @@ function initFilterButtons(chart, unfilteredData) {
       filterChartData(chart, unfilteredData, filter);
       clearFilterButtonsActiveClass();
       filterButton.classList.add('filterButton--active');
-    })
+    });
   });
 }
 
 /**
- * Remove the --active modifier from all filter buttons.
+ * Remove the `--active` modifier from all filter buttons.
  */
 function clearFilterButtonsActiveClass() {
   let filterButtons = document.querySelectorAll('.filterButton');
-  filterButtons.forEach(filterButton => filterButton.classList.remove('filterButton--active'));
+  filterButtons.forEach(
+      filterButton => filterButton.classList.remove('filterButton--active'));
 }
