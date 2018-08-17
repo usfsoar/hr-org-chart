8d7fdae72eea0481fcb475826dafa9a027841bed
Ian Sanders <iansan5653@gmail.com>
2018-08-17 01:29:08 -0400

    Add filtering capacity

--- org-chart.js
+++ org-chart.js
@@ -27,7 +27,6 @@ function loadData() {
  * @param {google.visualization.QueryResponse} response The Query response.
  */
 function handleDataResponse(response) {
-  console.log(typeof response);
   let data = response.getDataTable();
   let newColumnIndex = data.getNumberOfColumns();
   data.addColumn('string', 'OrgChartNodeContent');
@@ -47,28 +46,87 @@ function handleDataResponse(response) {
     data.setCell(row, newColumnIndex, data.getValue(row, COL_INDICES.UNIQUE_NAME), formatted);
   }
 
-  let orgChartDataView = new google.visualization.DataView(data);
-  orgChartDataView.setColumns([
+  let unfilteredDataView = new google.visualization.DataView(data);
+  unfilteredDataView.setColumns([
     newColumnIndex,
     COL_INDICES.RESPORTS_TO,
-    COL_INDICES.HELD_BY
+    COL_INDICES.HELD_BY,
+    COL_INDICES.TEAM // For filtering
   ]);
 
-  drawOrgChart(orgChartDataView);
+  initOrgChart(unfilteredDataView);
+}
+
+/**
+ * Create the org chart object.
+ * @param {google.visualization.DataView} data
+ */
+function initOrgChart(unfilteredData) {
+  const container = document.getElementById(CONTAINER_ID);
+  const orgChart = new google.visualization.OrgChart(container);
+
+  drawOrgChart(orgChart, unfilteredData);
+  initFilterButtons(orgChart, unfilteredData);
 }
 
 /**
  * Draw the chart using the data from the filter.
+ * @param {google.visualization.OrgChart} chart The chart object.
  * @param {google.visualization.DataView} data
  */
-function drawOrgChart(data) {
-  const container = document.getElementById(CONTAINER_ID);
-  const orgChart = new google.visualization.OrgChart(container);
-
-  orgChart.draw(data, {
+function drawOrgChart(chart, data) {
+  chart.draw(data, {
     allowCollapse: true,
     allowHtml: true,
     nodeClass: 'orgChart-node',
     selectedNodeClass: 'orgChart-node--selected'
   });
+}
+
+/**
+ * Filter the data by team name and redraw chart.
+ * @param {google.visualization.OrgChart} chart The chart object.
+ * @param {google.visualization.DataView} originalDataView The chart's unfiltered
+ * data view.
+ * @param {?string} [teamName] The exact name of the team to keep showing.
+ */
+function filterChartData(chart, unfilteredData, teamName) {
+  let filteredDataView = new google.visualization.DataView(unfilteredData);
+
+  if(teamName) {
+    let matchingRows = filteredDataView.getFilteredRows([{
+      column: 3,
+      value: teamName,
+    }]);
+    filteredDataView.setRows(matchingRows);
+  }
+
+  drawOrgChart(chart, filteredDataView);
+}
+
+/**
+ * Initalize the filter buttons by binding event listeners.
+ * @param {google.visualization.OrgChart} chart The chart object.
+ * @param {google.visualization.DataView} unfilteredData The chart's unfiltered
+ * data view.
+ */
+function initFilterButtons(chart, unfilteredData) {
+  let filterButtons = document.querySelectorAll('.filterButton');
+
+  filterButtons.forEach(filterButton => {
+    filterButton.addEventListener('click', event => {
+      let filter = event.target.dataset.filter;
+      filterChartData(chart, unfilteredData, filter);
+      clearFilterButtonsActiveClass();
+      filterButton.classList.add('filterButton--active');
+    })
+  });
+}
+
+/**
+ * Remove the --active modifier from all filter buttons.
+ */
+function clearFilterButtonsActiveClass() {
+  let filterButtons = document.querySelectorAll('.filterButton');
+  filterButtons.forEach(filterButton => filterButton.classList.remove('filterButton--active'));
 }
